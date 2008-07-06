d2bc46fd5c06e4baa916560ba9ddc9beb72c50c0
Björn Wolf <bjoern@we5.de>
2008-07-06 22:31:56 +0200

    Fixed file detection

--- lib/config/template_finder.rb
+++ lib/config/template_finder.rb
@@ -6,7 +6,7 @@ class ActionView::TemplateFinder
     return generic_base_path if generic_base_path
     path = File.expand_path(File.dirname(__FILE__)) + "/../../frontends/default/views" # this path references the list_filter directories, not the active scaffold directories
     # Should be able to use a rails method here to do this directory search
-    file = Dir.entries(path).find {|f| f =~ /^_?#{template_file_name}\.?#{extension}/ }
+    file = Dir.entries(path).find {|f| f =~ /^_?#{File.basename(template_file_name)}\.?#{extension}/ }
     file ? File.join(path, file) : nil
   end
   alias_method_chain :find_generic_base_path_for, :list_filter
